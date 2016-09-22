bee1061832e50dc7c8470da602651d159406f194
Rick Bacci <rickbacci@gmail.com>
2016-09-21 22:22:18 -0400

    add IssueCard component
    This breaks a few things but i'm going
    to roll with it for a bit to see where this leads.
    
    I need to rethink the way I structured the
    columns. Maybe I could use lodash to group them?
    
    I plan to refactor the IssueCard component into
    smaller pieces, but it helped to refactor using a
    similar structure as the original view.

---
+++ app/assets/javascripts/components/issue_card.js.jsx
@@ -0,0 +1,44 @@
+var IssueCards = React.createClass({
+  propTypes: {
+    issues: React.PropTypes.array,
+    status: React.PropTypes.string
+  },
+
+  render: function() {
+    var status     = this.props.status;
+    var issues     = this.props.issues;
+
+    var issuesList = issues.map(function(issue, index){
+      var milestone;
+
+      if (issue.milestone !== null) {
+        milestone = issue.milestone.title;
+      } else {
+        milestone = 'No milestone';
+      }
+
+      var labels = issue.labels.map(function(label, index){
+        return <li key={ index } style={{ backgroundColor: '#' + label.color }}>{ label.name }</li>
+      });
+
+      return (
+        <li key={ index } className='draggable panel panel-default card-panel cards' data-number={issue.number}>
+          <div className='panel-heading'>
+            <h3 className='panel-title pull-right'>#{ issue.number }</h3>
+            <h3 className='panel-title text-left'>{ milestone }</h3>
+          </div>
+          <div className='panel-body'>
+            <form className='well issue-card-body'>
+              <textarea defaultValue={ issue.title } rows={1} className={'form-control noscrollbars ctrl-enter title-' + issue.number}></textarea>
+              <textarea defaultValue={ issue.body } rows={5} className={'form-control noscrollbars ctrl-enter body-' + issue.number}></textarea>
+            </form>
+          </div>
+          <ul className='clearfix list-unstyled'>{ labels }</ul>
+        </li>
+      );
+
+    });
+
+    return  <ul className='status-list sortable list-unstyled' data-columnstatus={ status }>{ issuesList }</ul>;
+  }
+});
