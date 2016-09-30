511e25e16d06f575800e20a0cb40473329fe1eb6
Rick Bacci <rickbacci@gmail.com>
2016-09-30 09:27:59 -0400

    add state to IssueCardBody
    While i think this is the right place for this
    I am not sure that I needed to use state in this
    situation. The textarea is already updated, and
    the AJAX request needs to happen, but the card
    doesn't need to be re-rendered.
    
    - clean up code

--- app/assets/javascripts/components/issue_card_body.js.jsx
+++ app/assets/javascripts/components/issue_card_body.js.jsx
@@ -1,9 +1,57 @@
 var IssueCardBody = React.createClass({
   propTypes: {
-    title: React.PropTypes.string,
-    body: React.PropTypes.string
+    title:  React.PropTypes.string,
+    body:   React.PropTypes.string,
+    number: React.PropTypes.number,
+    labels: React.PropTypes.array
   },
+  getInitialState: function() {
+    var issueData = {
+      issueTitle: this.props.title,
+      issueBody:  this.props.body
+    };
+    return {
+      issueData: issueData
+    };
+  },
+  handleUserInput: function(issueTitle, issueBody) {
+    var url       = '/update_issues/' + this.props.number.toString()
+    var issueData = {
+      number: this.props.number,
+      title:  issueTitle.value,
+      body:   issueBody.value,
+      labels: this.props.labels
+    };
 
+    $.ajax({
+      url: url,
+      method: 'patch',
+      data: {
+        number: issueData.number,
+        title:  issueData.title,
+        body:   issueData.body,
+        labels: issueData.labels
+      },
+      dataType: 'json',
+      cache: false,
+      success: function() {
+        this.setState({
+          issueTitle: issueData.title,
+          issueBody:  issueData.body
+        });
+        $.notify({
+          message: 'Issue Update Successful!'
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        $.notify({
+          message: 'Issue Update Failed!'
+        });
+        console.error(url, status, err.toString());
+      }.bind(this)
+
+    });
+  },
   render: function() {
     return (
       <div className='panel-body'>
@@ -14,4 +62,5 @@ var IssueCardBody = React.createClass({
       </div>
     );
   }
+
 });
