3c45040d8dfef9f755f111b415a66a5457205e2e
Rick Bacci <rickbacci@gmail.com>
2016-09-30 11:39:06 -0400

    cleanup IssueCardLabels

--- app/assets/javascripts/components/issue_labels_list.js.jsx
+++ app/assets/javascripts/components/issue_labels_list.js.jsx
@@ -2,17 +2,20 @@ var IssueLabelsList = React.createClass({
   propTypes: {
     labels: React.PropTypes.array
   },
-
   render: function() {
-
-
     var issueLabelListItems = this.props.labels.map(function(label, index){
-      return <li className='btn btn-xs' key={ index } style={{ backgroundColor: '#' + label.color }}>{ label.name }</li>
+
+      var style = {backgroundColor: '#' + label.color};
+      return (
+        <li className='btn btn-xs' key={index} style={style}>
+          {label.name}
+        </li>
+      );
     });
 
     return (
       <div className='issue-labels'>
-        <ul className='list-unstyled'>{ issueLabelListItems }</ul>
+        <ul className='list-unstyled'>{issueLabelListItems}</ul>
       </div>
     );
   }
