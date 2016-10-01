b0e067ff8b36663ffb7411e14236a6c596bbda3e
Rick Bacci <rickbacci@gmail.com>
2016-09-30 21:36:39 -0400

    refactor IssueCardDropdowns

--- app/assets/javascripts/components/issue_card_dropdowns.js.jsx
+++ app/assets/javascripts/components/issue_card_dropdowns.js.jsx
@@ -1,16 +1,22 @@
 var IssueCardDropdowns = React.createClass({
   propTypes: {
-    repoLabels: React.PropTypes.array,
+    repoLabels:  React.PropTypes.array,
     issueLabels: React.PropTypes.array,
-    issueTime: React.PropTypes.string
+    issueTime:   React.PropTypes.string
   },
-
   render: function() {
     return (
       <div className='issue-dropdowns clearfix'>
-        <UpdateLabelsDropdown repoLabels={ this.props.repoLabels } issueLabels={ this.props.issueLabels } />
-        <UpdateTimeDropdown issueTime={ this.props.issueTime } />
+        <form>
+          <UpdateLabelsDropdown
+            repoLabels  = {this.props.repoLabels}
+            issueLabels = {this.props.issueLabels}
+            onUserInput = {this.props.onUserInput}
+          />
+          <UpdateTimeDropdown issueTime={ this.props.issueTime } />
+        </form>
       </div>
     );
   }
+
 });
